4cbef63bd8f188eeb539fd9d0df7e39f2c822738
Justin Seaward <jut_seaward@yahoo.com>
2026-03-10 19:52:44 -0230

    created a new MoveablePoint object, printed its data. Used the move() method on myPoint and printed the results.

--- src/Problem2/Demo.java
+++ src/Problem2/Demo.java
@@ -1,4 +1,14 @@
 package Problem2;
 
 public class Demo {
+    public static void main (String[] args){
+
+        MovablePoint myPoint = new MovablePoint(10,20,50,60);
+        System.out.println("My Point: " + myPoint);
+
+        myPoint.move();
+        System.out.println("My Point moved: " + myPoint);
+
+
+    }
 }
