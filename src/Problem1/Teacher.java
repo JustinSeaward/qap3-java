8d327c168e35263c208f097ed6c2c5b681909ca8
Justin Seaward <jut_seaward@yahoo.com>
2026-03-09 22:10:28 -0230

    Tescher: Added variables and  constructor for Teacher. Added getters and setters for mySubject, mySalary and a toString method.4

--- src/Problem1/Teacher.java
+++ src/Problem1/Teacher.java
@@ -1,4 +1,34 @@
 package Problem1;
 
-public class Teacher {
+public class Teacher extends Person{
+    protected String mySubject;
+    protected double mySalary;
+
+    public Teacher(String name, int age, String gender, String subject, double salary){
+        super(name,age,gender);
+        mySubject = subject;
+        mySalary = salary;
+    }
+
+    public double getMySalary() {
+        return mySalary;
+    }
+
+    public void setMySalary(double salary) {
+        this.mySalary = salary;
+    }
+
+    public String getMySubject() {
+        return mySubject;
+    }
+
+    public void setMySubject(String subject) {
+        this.mySubject = subject;
+    }
+
+    public String toString(){
+            return super.toString() + ", Subject: " + mySubject + ", Salary: " + mySalary;
+    }
+
+
 }
