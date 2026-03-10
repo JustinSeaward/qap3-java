1fdb715d2dace5b93b1f452ae82286077942e3c4
Justin Seaward <jut_seaward@yahoo.com>
2026-03-10 18:18:43 -0230

    added variables. added constructors

--- src/Problem2/MovablePoint.java
+++ src/Problem2/MovablePoint.java
@@ -1,4 +1,23 @@
 package Problem2;
 
-public class MovablePoint {
+public class MovablePoint extends Point {
+    private float xSpeed = 0.0f;
+    private float ySpeed = 0.0f;
+
+    public MovablePoint(float x, float y, float xSpeed, float ySpeed){
+        super(x,y);
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    public MovablePoint(float xSpeed, float ySpeed){
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    public MovablePoint(){
+
+    }
+
+
 }
