494d70f8e21891b37d0bebbde985cef6ee1b8f31
Justin Seaward <jut_seaward@yahoo.com>
2026-03-10 20:40:07 -0230

    Shape: input all logic for the abstract Shape class. name veriable, constructors, getters, setters. getArea() and getPerimeter abstract methods and toString method

--- src/Problem3/Shape.java
+++ src/Problem3/Shape.java
@@ -1,4 +1,28 @@
 package Problem3;
 
-public class Shape {
+public abstract class Shape {
+    private String name;
+
+    public Shape(){
+    }
+
+    public Shape(String name){
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public abstract double getArea();
+
+    public abstract double getPerimeter();
+
+    public String toString(){
+        return "Name: " + this.name + ", Area: " + this.getArea() + ", Perimeter: " + this.getPerimeter();
+    }
 }
