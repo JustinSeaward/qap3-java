9142b50abf1a3e32913ec1a1aa69abc15d6d6eb5
Justin Seaward <jut_seaward@yahoo.com>
2026-03-10 22:54:42 -0230

    Triangle: added variables for three sides. added constructors, getters ans setters for each side. added getArea() and getPerimeter() methods.

--- src/Problem3/Triangle.java
+++ src/Problem3/Triangle.java
@@ -1,4 +1,62 @@
 package Problem3;
 
-public class Triangle {
+public class Triangle extends Shape{
+    private double sideA;
+    private double sideB;
+    private double sideC;
+
+    public Triangle(){
+
+    }
+
+    public Triangle(String name, double sideA, double sideB, double sideC){
+        super(name);
+        if(sideA + sideB > sideC && sideB + sideC > sideA && sideC + sideA > sideB){
+            this.sideA = sideA;
+            this.sideB = sideB;
+            this.sideC = sideC;
+        } else {
+            System.out.println("Values will not make a triangle");
+            System.exit(1);
+        }
+
+    }
+
+    public double getSideA() {
+        return sideA;
+    }
+
+    public void setSideA(double sideA) {
+        this.sideA = sideA;
+    }
+
+    public double getSideB() {
+        return sideB;
+    }
+
+    public void setSideB(double sideB) {
+        this.sideB = sideB;
+    }
+
+    public double getSideC() {
+        return sideC;
+    }
+
+    public void setSideC(double sideC) {
+        this.sideC = sideC;
+    }
+
+    @Override
+    public double getArea() {
+        double semiPerimeter;
+        semiPerimeter = (this.sideA + this.sideB + this.sideC) / 2;
+        return Math.sqrt(semiPerimeter*(semiPerimeter-this.sideA)*(semiPerimeter-this.sideB)*(semiPerimeter-sideC));
+    }
+
+    @Override
+    public double getPerimeter() {
+        return this.sideA + this.sideB + this.sideC;
+
+    }
+
 }
