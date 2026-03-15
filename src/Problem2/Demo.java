766ecc0588f68179942d53cfc2a4c70ebd299e20
Justin Seaward <jut_seaward@yahoo.com>
2026-03-15 15:23:06 -0230

    add space on line 7

--- src/Problem2/Demo.java
+++ src/Problem2/Demo.java
@@ -4,6 +4,7 @@ public class Demo {
     public static void main (String[] args){
 
         MovablePoint myPoint = new MovablePoint(10,20,50,60);
+        System.out.println();
         System.out.println("My Point: " + myPoint);
 
         myPoint.move();
